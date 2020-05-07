e6b17cd3adef21a59f6a2827b9b24ada826b9e82
taigalee <taiga@taigalee.com>
2020-05-06 22:24:20 -0500

    Server set up

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const PORT = process.env.PORT;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+http.listen(PORT, () => {
+  const d = new Date();
+  console.log(`${d.toLocaleString()}: Server listening on port ${PORT}`);
+});
