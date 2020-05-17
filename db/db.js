2bfd656e9c89856888e27bb5829f4aafe42db22d
taigalee <taiga@taigalee.com>
2020-05-16 21:56:14 -0500

    mongodb

--- db/db.js
+++ db/db.js
@@ -2,10 +2,20 @@ const mongoose = require("mongoose");
 
 const connectionString = process.env.MONGODB_URI;
 
+const MongoClient = require("mongodb").MongoClient;
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  client.close();
+});
+
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 mongoose.connection.on("connected", () => {
@@ -16,7 +26,7 @@ mongoose.connection.on("disconnected", () => {
   console.log("Disconnected from butterhalf db");
 });
 
-mongoose.connection.on("error", err => {
+mongoose.connection.on("error", (err) => {
   console.log("Error with database connection");
   console.log(err);
 });
